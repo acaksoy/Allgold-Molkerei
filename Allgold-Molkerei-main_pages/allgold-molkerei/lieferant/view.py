925f77cee3e25177c73991de6d4cfb172dde1946
61celil <celil.sari@stud.hs-kempten.de>
2021-06-07 21:42:43 +0200

    lieferanterfassen, verkauferfassen fertig

--- Allgold-Molkerei-main_pages/allgold-molkerei/lieferant/view.py
+++ Allgold-Molkerei-main_pages/allgold-molkerei/lieferant/view.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from database import Lieferung
+from datetime import datetime
+from database import db
 
 Lieferant = Blueprint('lieferant', __name__, template_folder='pages', static_folder='static')
 
@@ -7,6 +10,14 @@ Lieferant = Blueprint('lieferant', __name__, template_folder='pages', static_fol
 def home():
     return render_template('lieferant.html')
 
-@Lieferant.route('/lieferant/erfassen')
+@Lieferant.route('/erfassen', methods=['GET','POST'])
 def erfassung():
+    if request.method == "POST":
+       date = datetime.strptime(request.form['lfDat'], '%Y-%m-%d')
+       lieferfass = Lieferung(date, request.form['mg'], request.form['name'], request.form['prID'], request.form['vkID'])
+       db.session.add(lieferfass)
+       db.session.commit()
+       return redirect(url_for("lieferant.home"))
     return render_template('lieferf.html')
+
+
--- Allgold-Molkerei-main_pages/allgold-molkerei/verkaeufer/view.py
+++ Allgold-Molkerei-main_pages/allgold-molkerei/verkaeufer/view.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template,request, flash
+from database import Elements, Verkauf
+from datetime import datetime
 Verkaeufer = Blueprint('verkaeufer', __name__, template_folder='pages', static_folder='static', static_url_path='/verkaeufer/static')
 
 # all routes
@@ -8,14 +9,28 @@ def home():
 
     return render_template('verkaeufer.html')
 
-@Verkaeufer.route('/verkaeufer/erfassen')
+@Verkaeufer.route('/erfassen', methods=['GET','POST'])
 def erfassen():
-
+    if request.method == "POST" and request.form['vkID'] is not "":
+       date = datetime.strptime(request.form['vkDat'], '%Y-%m-%d')
+       verkerf = Verkauf(request.form['vkID'], request.form['prID'],request.form['mg'],date)
+       db.session.add(verkerf)
+       db.session.commit()
+       flash("Lieferung erfolgreich erfasst")
+       return redirect(url_for("verkaeufer.erfassen", verkerf=verkerf))
+    else:
+       flash("Gültige Verkaufsstellen ID eingeben!")
     return render_template('verkerf.html')
 
 @Verkaeufer.route('/verkaeufer/prodErfassen')
 def proderfassen():
-
+    if request.method == "POST":
+       date = datetime.strptime(request.form['lfDat'], '%Y-%m-%d')
+       proderfassen = Elements(request.form['name'], date, request.form['mg'],  request.form['prID'],)
+       db.session.add(lieferfass)
+       db.session.commit()
+       flash("Lieferung erfolgreich erfasst")
+       return render_template("test1.html", proderfassen=proderfassen)
     return render_template('proderf.html')
 
 
