4dff2fd909eec98f3faa0979cfb9dc14ccf2e0b9
61celil <celil.sari@stud.hs-kempten.de>
2021-06-09 22:07:47 +0200

    verkauferfassen not working

--- Allgold-Molkerei-main_pages/allgold-molkerei/verkaeufer/view.py
+++ Allgold-Molkerei-main_pages/allgold-molkerei/verkaeufer/view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template,request, flash, redirect, url_for
-from database import Elements, Verkauf,Produkt, db
+from database import Elements, Verkauf,Produkt, db, Verkaufstelle,Inventar
 from datetime import datetime
 Verkaeufer = Blueprint('verkaeufer', __name__, template_folder='pages', static_folder='static', static_url_path='/verkaeufer/static')
 
@@ -13,10 +13,23 @@ def home():
 def erfassen():
     if request.method == "POST":
        date = datetime.strptime(request.form['vkDat'], '%Y-%m-%d').date()
-       verkerf = Verkauf(date,request.form['mg'],request.form['prID'],request.form['vkID'])
+       menge = int(request.form['mg'])
+
+       verkerf = Verkauf(date,request.form['mg'],request.form['prID'],int(request.form['vkID']))
        db.session.add(verkerf)
        db.session.commit()
-       flash('Lieferung erfolgreich erfasst!', 'success')
+
+       pID = int(request.form['prID'])
+       verkstelle = Verkaufstelle.query.get(verkerf.verkaufstelleID)
+       invID = verkstelle.inventar_ID
+       INV = Inventar.query.get(invID)
+
+       #for x in range(menge):
+       element = Elements.query.filter_by(inventar_ID = INV, produktID = pID).first()
+       db.session.delete(element)
+       db.session.commit()
+
+
        return render_template("verkaeufer.html")
        #return redirect(url_for(".home", verkerf=verkerf))
     else:
